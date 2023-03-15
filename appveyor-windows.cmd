c2d6737723af76786360b2fea4007d08362fd329
cqm <mtdxc@126.com>
2023-03-15 13:16:41 +0800

    add cpp_demo

--- appveyor-windows.cmd
+++ appveyor-windows.cmd
@@ -15,9 +15,9 @@ git submodule init
 
 git submodule update --checkout --force --recursive
 
-set VCPKG_ROOT=c:/tools/vcpkg/
+set VCPKG_ROOT=D:/vcpkg/
 
-cmake -DCMAKE_GENERATOR_PLATFORM=x64 -DBUILD_SHARED_LIBS=OFF -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=c:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake -DGETOPT_INCLUDE_DIR=c:/tools/vcpkg/installed/x64-windows/include  -DGETOPT_LIB=c:/tools/vcpkg/installed/x64-windows/lib/getopt.lib -DBORINGSSL_DIR=%cd%\boringssl .
+cmake -DCMAKE_GENERATOR_PLATFORM=x64 -DBUILD_SHARED_LIBS=OFF -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=D:/vcpkg/scripts/buildsystems/vcpkg.cmake -DGETOPT_INCLUDE_DIR=D:/vcpkg/installed/x64-windows/include  -DGETOPT_LIB=D:/vcpkg/installed/x64-windows/lib/getopt.lib -DZLIB_INCLUDE_DIR=D:/vcpkg/installed/x64-windows/include -DBORINGSSL_DIR=%cd%\boringssl .
 
 
 msbuild /m ALL_BUILD.vcxproj
